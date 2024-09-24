7b11bff9064c71bf524832c11f717c618848386a
Brad-Hammond <brad_hammond1@sky.com>
2024-09-24 15:32:23 +0100

    Feat: Added signup function to bookigns/views.

--- bookings/views.py
+++ bookings/views.py
@@ -5,3 +5,14 @@ from .models import Booking
 from .forms import BookingForm, FeedbackForm
 
 # Create your views here.
+
+# View for user signup
+def signup(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('login')  # Redirect to login page after signup
+    else:
+        form = UserCreationForm()
+    return render(request, 'signup.html', {'form': form})
