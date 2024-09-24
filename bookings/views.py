78253aaa8ca0be19771542153518b038d3d05c7d
Brad-Hammond <brad_hammond1@sky.com>
2024-09-24 15:23:23 +0100

    Feat: Add view utilities, auth decorators, - UserCreationForm, BookingForm, and FeedbackForm.

--- bookings/views.py
+++ bookings/views.py
@@ -1,3 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
+from django.contrib.auth.decorators import login_required, permission_required
+from django.contrib.auth.forms import UserCreationForm  # Import for signup
+from .models import Booking
+from .forms import BookingForm, FeedbackForm
 
 # Create your views here.
