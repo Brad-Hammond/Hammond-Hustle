44151707e8a40c2b58b7be4c552bc3e379a85d40
Brad-Hammond <brad_hammond1@sky.com>
2024-10-07 15:22:44 +0100

    Feat: Added html templates to project urls.

--- gym_website/urls.py
+++ gym_website/urls.py
@@ -16,8 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from bookings import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('bookings/', include('bookings.urls')),
+    path('', views.home, name='home'),  # Root URL redirects to the home view, rendering base.html
 ]
