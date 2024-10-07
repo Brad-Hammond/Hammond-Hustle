31da2c43808dde5dfaf799d93efa0c2cb306ee7b
Brad-Hammond <brad_hammond1@sky.com>
2024-10-07 12:09:39 +0100

    Feat: Added the signup func to bookings.views.

--- bookings/views.py
+++ bookings/views.py
@@ -1,18 +1,34 @@
+from django.contrib.auth import login
 from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.decorators import login_required, permission_required
 from django.contrib.auth.forms import UserCreationForm  # Import for signup
 from .models import Booking
-from .forms import BookingForm, FeedbackForm
+from .forms import BookingForm, FeedbackForm, CustomUserCreationForm
 
 # Create your views here.
 
 # View for user signup
 def signup(request):
     if request.method == 'POST':
-        form = UserCreationForm(request.POST)
+        form = CustomUserCreationForm(request.POST)
         if form.is_valid():
-            form.save()
-            return redirect('login')  # Redirect to login page after signup
+            user = form.save()
+
+            
+            code = form.cleaned_data.get('code')
+
+            
+            if code == 'EMPLOYEE2024':
+                employee_group = Group.objects.get(name='Employee')
+                user.groups.add(employee_group)
+            else:
+                user_group = Group.objects.get(name='User')
+                user.groups.add(user_group)
+
+            
+            login(request, user)
+            return redirect('home')
     else:
-        form = UserCreationForm()
-    return render(request, 'signup.html', {'form': form})
+        form = CustomUserCreationForm()
+    
+    return render(request, 'accounts/signup.html', {'form': form})
