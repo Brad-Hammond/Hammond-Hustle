16d1eb0cf4b6b4e6609795a8df5fb186d397896d
Brad-Hammond <brad_hammond1@sky.com>
2024-10-09 16:24:54 +0100

    Feat: Added redirect to homepage in settings.py.

--- gym_website/settings.py
+++ gym_website/settings.py
@@ -19,6 +19,8 @@ if os.path.isfile('env.py'):
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+LOGIN_REDIRECT_URL = '/' 
+
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
