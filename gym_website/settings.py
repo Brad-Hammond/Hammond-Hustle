7c5245d5b692c31ad74de1ea9395679ea607591b
Brad-Hammond <brad_hammond1@sky.com>
2024-10-09 16:36:51 +0100

    Feat: Redirectd logout to homepage.

--- gym_website/settings.py
+++ gym_website/settings.py
@@ -19,7 +19,8 @@ if os.path.isfile('env.py'):
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-LOGIN_REDIRECT_URL = '/' 
+LOGIN_REDIRECT_URL = '/'
+LOGOUT_REDIRECT_URL = '/'  # Redirects to the homepage URL
 
 
 # Quick-start development settings - unsuitable for production
