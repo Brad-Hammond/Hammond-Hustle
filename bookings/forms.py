428ae9b47bc7fc0e6dd9be13cedb85e2f57e1942
Brad-Hammond <brad_hammond1@sky.com>
2024-10-09 20:56:23 +0100

    Feat: Amended forms.py to try to fix bookings - being assigned correctly.

--- bookings/forms.py
+++ bookings/forms.py
@@ -1,9 +1,9 @@
+# bookings/forms.py
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 from .models import Booking  # Import the Booking model
 
-# Form for booking creation
 class BookingForm(forms.ModelForm):
     class Meta:
         model = Booking
@@ -12,15 +12,22 @@ class BookingForm(forms.ModelForm):
             'session_time': forms.DateTimeInput(attrs={'type': 'datetime-local'}),
         }
 
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        # Populate choices for 'coach' field based on COACH_CHOICES in Booking model
+        self.fields['coach'].choices = Booking.COACH_CHOICES
+
 # Form for employee feedback on a booking
 class FeedbackForm(forms.ModelForm):
     class Meta:
         model = Booking
         fields = ['feedback']  # Only allow employees to edit the feedback field
 
-# Custom signup form with employee code field
+# Custom signup form with an optional employee code field
 class CustomUserCreationForm(UserCreationForm):
-    code = forms.CharField(max_length=20, required=False, help_text='Enter your code if you are an employee.')
+    code = forms.CharField(
+        max_length=20, required=False, help_text='Enter your code if you are an employee.'
+    )
 
     class Meta:
         model = User
